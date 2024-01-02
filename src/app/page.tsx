62158e03efbd60c81531564c248a43824c66fc30
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-03 01:15:45 +0900

    🔧 : パスエイリアス追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { Button } from '@/app/_components/ui/button';
+import { Button } from '@app/_components/ui/button';
 
 export default function Home() {
   return (
