1f4076a54a2050041d6a81334075d7fc37aab15e
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-04 18:32:14 +0900

    ✨ : ブログ一覧ページ実装

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -1,4 +1,25 @@
 import { createClient } from 'microcms-js-sdk';
+import type { MicroCMSQueries, MicroCMSImage, MicroCMSDate, MicroCMSContentId } from 'microcms-js-sdk';
+
+type Tag = {
+  name: string;
+} & MicroCMSContentId &
+  MicroCMSDate;
+
+type Author = {
+  name: string;
+  profile: string;
+  image?: MicroCMSImage;
+} & MicroCMSContentId &
+  MicroCMSDate;
+
+export type Blog = {
+  title: string;
+  content: string;
+  thumbnail?: MicroCMSImage;
+  tags?: Tag[];
+  author?: Author;
+};
 
 if (!process.env.MICRO_CMS_SERVICE_DOMAIN) {
   throw new Error('MICRO_CMS_SERVICE_DOMAIN is required');
@@ -10,3 +31,15 @@ export const client = createClient({
   serviceDomain: process.env.MICRO_CMS_SERVICE_DOMAIN,
   apiKey: process.env.MICRO_CMS_API_KEY,
 });
+
+/**
+ * ブログ一覧を取得する関数
+ */
+export const getBlogs = async (queries?: MicroCMSQueries) => {
+  const blogData = await client.getList<Blog>({
+    endpoint: 'blogs',
+    queries,
+  });
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  return blogData;
+};
