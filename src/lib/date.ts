f67b360e80925f979c02424b133758edccc04a60
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-04 19:11:01 +0900

    ⚡️ : Date.parseを二回呼び出さないように調整

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -2,10 +2,10 @@ import { format } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
 
 export const formatDate = (date: string) => {
-  if (isNaN(Date.parse(date))) {
+  const utcDate = new Date(date);
+  if (isNaN(utcDate.getTime())) {
     throw new Error('Invalid date format');
   }
-  const utcDate = new Date(date);
   const jstDate = utcToZonedTime(utcDate, 'Asia/Tokyo');
   return format(jstDate, 'yyyy-MM-dd');
 };
