932397ada4c4142000dea59c591e1d684c2b1882
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-04 13:55:25 +0900

    ✨ : APIを叩いて、テキストを表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image';
+import { client } from '@/lib/microcms';
 import { Button } from '@app/_components/ui/button';
 
-export default function Home() {
+export default async function Home() {
+  const data = await client.get({
+    endpoint: 'test',
+  });
+
+  console.log(data);
+
   return (
     <>
       <h1>こんにちは</h1>
+      <div>{data.text}</div>
       <Button>button</Button>
     </>
   );
