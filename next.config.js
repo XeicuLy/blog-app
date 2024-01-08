60f2281cab6ccbb22a52e3922cb7c18cd6c5cf30
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-08 20:27:43 +0900

    🛂 : Next14の書き方に修正

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    remotePatterns: ['images.microcms-assets.io/**'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
