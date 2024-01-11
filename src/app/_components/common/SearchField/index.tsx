f2dc8858ffa0dbc31278359dfe360a5be5919f1a
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-11 22:10:32 +0900

    💄 : 検索ボタン追加

--- src/app/_components/common/SearchField/index.tsx
+++ src/app/_components/common/SearchField/index.tsx
@@ -4,6 +4,7 @@ import { useSearchParams } from 'next/navigation';
 import { useCallback, useRef, useState } from 'react';
 import { HiOutlineSearch } from 'react-icons/hi';
 
+import { Button } from '@/app/_components/ui/button';
 import { Input } from '@/app/_components/ui/input';
 
 const SearchField = () => {
@@ -22,20 +23,31 @@ const SearchField = () => {
   const searchParams = useSearchParams();
   const defaultQuery = searchParams.get('q') || '';
 
+  const handleClick = () => {
+    if (!composing) {
+      location.href = `/search?q=${inputRef.current?.value}`;
+    }
+  };
+
   return (
-    <div className='relative'>
-      <Input
-        type='search'
-        name='q'
-        ref={inputRef}
-        placeholder='タグを検索する'
-        onKeyDown={_onEnter}
-        onCompositionStart={startComposition}
-        onCompositionEnd={endComposition}
-        defaultValue={defaultQuery}
-        className='pl-8' // アイコンの幅に応じて調整
-      />
-      <HiOutlineSearch className='absolute left-1.5 top-1/2 -translate-y-1/2 transform' />
+    <div className='flex'>
+      <div className='relative w-full'>
+        <Input
+          type='search'
+          name='q'
+          ref={inputRef}
+          placeholder='タグを検索する'
+          onKeyDown={_onEnter}
+          onCompositionStart={startComposition}
+          onCompositionEnd={endComposition}
+          defaultValue={defaultQuery}
+          className='pl-8' // アイコンの幅に応じて調整
+        />
+        <HiOutlineSearch className='absolute left-1.5 top-1/2 -translate-y-1/2 transform' />
+      </div>
+      <Button className='ml-4 bg-green-200 text-gray-500 duration-150 hover:bg-green-300' onClick={handleClick}>
+        検索
+      </Button>
     </div>
   );
 };
