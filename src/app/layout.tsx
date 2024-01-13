e3fae0f3f0079f2d2153043dc4937895c16a66a0
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-01-13 16:07:32 +0900

    🩹 : 不要なasync削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ type Props = {
   children: React.ReactNode;
 };
 
-export default async function RootLayout({ children }: Props) {
+export default function RootLayout({ children }: Props) {
   return (
     <html lang='ja'>
       <body className={notoSansJP.className}>{children}</body>
